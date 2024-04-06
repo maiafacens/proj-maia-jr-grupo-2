68f6f795289e88859815e51206529507108b7d0a
Gabriel Capelini de Oliveira <158757773+capeezed@users.noreply.github.com>
2024-04-06 08:38:53 -0300

    Update main.cpp

--- maiaJr/src/main.cpp
+++ maiaJr/src/main.cpp
@@ -1,147 +1,170 @@
-#include <NewPing.h>
 
-//variaveis
-#define pino1 6
-#define pino2 5
-#define ent1 12
-#define ent2 13
-#define ent3 3
-#define ent4 4
-#define max 400
-const int alertadis = 40;
-int distancia_E, distancia_C, distancia_D = 0;
+  //variaveis
+  #define pino1 6
+  #define pino2 5
+  #define ent1 13
+  #define ent2 12
+  #define ent3 3
+  #define ent4 4
+  #define trig1 10
+  #define ech1 9
+  #define trig2 2
+  #define ech2 7
+  #define trig3 8
+  #define ech3 11
+  int distancia_E, distancia_C, distancia_D;
+  const int alertadis = 15;
 
-//sensores
-NewPing sonar1(9, 9, max);
-NewPing sonar2(2, 2  , max);
-NewPing sonar3(11, 11, max);
+  //funcoes
+  int getd1();
+  int getd2();
+  int getd3();
+  void acelerar();
+  void parar();
+  void re();
+  void esquerda();
+  void direita();
 
-//funcoes
-int getd1();
-int getd2();
-int getd3();
-void acelerar();
-void parar();
-void re();
-void esquerda();
-void direita();
+  void setup()
+  {
 
-void setup()
-{
+    Serial.begin(9600);
+    Serial.println("Sistemas iniciando...");
+    pinMode(pino1, OUTPUT);
+    pinMode(pino2, OUTPUT);
+    pinMode(ent1, OUTPUT);
+    pinMode(ent2, OUTPUT);
+    pinMode(ent3, OUTPUT);
+    pinMode(ent4, OUTPUT);
+    pinMode(trig1,OUTPUT);
+    pinMode(trig2, OUTPUT);
+    pinMode(trig3,OUTPUT);
+    pinMode(ech1,INPUT);
+    pinMode(ech2,INPUT);
+    pinMode(ech3,INPUT);
 
-  Serial.begin(5000);
-  Serial.println("Sistemas iniciando...");
-  pinMode(pino1, OUTPUT);
-  pinMode(pino2, OUTPUT);
-  pinMode(ent1, OUTPUT);
-  pinMode(ent2, OUTPUT);
-  pinMode(ent3, OUTPUT);
-  pinMode(ent4, OUTPUT);
-
-}
-
-
-void loop()
-{
-
-distancia_E = getd1();
-distancia_C = getd2();
-distancia_D = getd3();
-
-  if((distancia_D <= alertadis) && (distancia_D > 5)) {
-        esquerda();
   }
 
-    else if((distancia_C <= alertadis) && (distancia_C > 5)) {
-        esquerda();
-  }
+
+  void loop()
+  {
+
+  distancia_E = getd1();
+  distancia_C = getd2();
+  distancia_D = getd3();
+
+    if((distancia_D <= alertadis) && (distancia_D > 5)) {
+          esquerda();
+    }
+
+      else if((distancia_C <= alertadis) && (distancia_C > 5)) {
+          esquerda();
+    }
+      
+        else if((distancia_E <= alertadis) && (distancia_E > 5)) {
+          direita();
+    }
     
-      else if((distancia_E <= alertadis) && (distancia_E > 5)) {
-        direita();
-  }
-  
-      else if((distancia_E > alertadis) && (distancia_C > alertadis) && (distancia_D > alertadis) ){
-        acelerar();
+        else if((distancia_E > alertadis) && (distancia_C > alertadis) && (distancia_D > alertadis) ){
+          acelerar();
+    }
+	
   }
 
-}
+  int getd1()
+  {
+    int distancia, duration;
+    digitalWrite(trig1,LOW);
+    digitalWrite(trig1, HIGH);
+    delayMicroseconds(2);
+    digitalWrite(trig1,LOW);
+    duration = pulseIn(ech1,HIGH);
+    distancia = duration / 58.2;
 
-int getd1()
-{
-  int distancia_E;
-  distancia_E = sonar1.ping_cm();
-  return distancia_E;
-}
+    return distancia;
+  }
 
-int getd2()
-{
-  int distancia_C;
-  distancia_C = sonar2.ping_cm();
-  return distancia_C;
-}
+  int getd2()
+  {
+    int distancia, duration;
+    digitalWrite(trig2,LOW);
+    digitalWrite(trig2, HIGH);
+    delayMicroseconds(2);
+    digitalWrite(trig2,LOW);
+    duration = pulseIn(ech2,HIGH);
+    distancia = duration / 58.2;
 
-int getd3()
-{
-  int distancia_D;
-  distancia_D = sonar3.ping_cm();
-  return distancia_D;
-}
+    return distancia;
+  }
 
-void acelerar()
-{
-  analogWrite(pino1, 500);
-  analogWrite(pino2, 500);
-  digitalWrite(ent1, LOW);
-  digitalWrite(ent2, HIGH);
-  digitalWrite(ent3, HIGH);
-  digitalWrite(ent4, LOW);
+  int getd3()
+  {
+    int distancia, duration;
+    digitalWrite(trig3,LOW);
+    digitalWrite(trig3, HIGH);
+    delayMicroseconds(2);
+    digitalWrite(trig3,LOW);
+    duration = pulseIn(ech3,HIGH);
+    distancia = duration / 58.2;
 
-}
+    return distancia;
+  }
 
-void parar()
-{
-
-  analogWrite(pino1, 500);
-  analogWrite(pino2, 500);
-  digitalWrite(ent1,LOW);
-  digitalWrite(ent2,LOW);
-  digitalWrite(ent3,LOW);
-  digitalWrite(ent4,LOW);
-
-}
-
-void re()
-{
-  
-  analogWrite(pino1, 500);
-  analogWrite(pino2, 500);
-  digitalWrite(ent1,HIGH);
-  digitalWrite(ent2,LOW);
-  digitalWrite(ent3,LOW);
-  digitalWrite(ent4,HIGH);
-
-}
-
-void direita()
-{
-  
-  analogWrite(pino1, 500);
-  analogWrite(pino2, 500);
-  digitalWrite(ent1,LOW);
-  digitalWrite(ent2,HIGH);
-  digitalWrite(ent3,LOW);
-  digitalWrite(ent4,LOW);
+  void acelerar()
+  {
+    analogWrite(pino1, 500);
+    analogWrite(pino2, 500);
+    digitalWrite(ent1, LOW);
+    digitalWrite(ent2, HIGH);
+    digitalWrite(ent3, HIGH);
+    digitalWrite(ent4, LOW);
 
   }
 
-void esquerda()
-{
-  
-  analogWrite(pino1, 500);
-  analogWrite(pino2, 500);
-  digitalWrite(ent1,LOW);
-  digitalWrite(ent2,LOW);
-  digitalWrite(ent3,HIGH);
-  digitalWrite(ent4,LOW);
+  void parar()
+  {
+
+    analogWrite(pino1, 500);
+    analogWrite(pino2, 500);
+    digitalWrite(ent1,LOW);
+    digitalWrite(ent2,LOW);
+    digitalWrite(ent3,LOW);
+    digitalWrite(ent4,LOW);
+
+  }
+
+  void re()
+  {
     
+    analogWrite(pino1, 500);
+    analogWrite(pino2, 500);
+    digitalWrite(ent1,HIGH);
+    digitalWrite(ent2,LOW);
+    digitalWrite(ent3,LOW);
+    digitalWrite(ent4,HIGH);
+
   }
+
+  void direita()
+  {
+    
+    analogWrite(pino1, 500);
+    analogWrite(pino2, 500);
+    digitalWrite(ent1,LOW);
+    digitalWrite(ent2,HIGH);
+    digitalWrite(ent3,LOW);
+    digitalWrite(ent4,LOW);
+
+    }
+
+  void esquerda()
+  {
+    
+    analogWrite(pino1, 500);
+    analogWrite(pino2, 500);
+    digitalWrite(ent1,LOW);
+    digitalWrite(ent2,LOW);
+    digitalWrite(ent3,HIGH);
+    digitalWrite(ent4,LOW);
+      
+    }
